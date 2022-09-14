2ba5127f7994f8eb3a4736e382da20ac7a11b60d
Dikran H <dhachikian@expert-bg.org>
2022-09-14 10:01:31 +0300

    Python L01

---
+++ ex04_zan01.py
@@ -0,0 +1,27 @@
+# %%
+s1 = 'Lorem ipsum dolor sit amet, consectetur'
+
+s2 = "Lorem ipsum dolor sit amet, consectetur"
+
+# %%
+
+s3 = '''Lorem ipsum dolor
+sit amet, consectetur adipisicing elit, 
+sed do eiusmod tempor incididunt ut 
+labore et dolore'''
+
+print(s3)
+
+# %%
+def foo():
+    '''Function foo print Hello Python
+Created: 2022-09-12
+Author: Me
+'''
+    print('Hello Python')
+
+# %%
+foo()
+# %%
+print(foo.__doc__)
+# %%
