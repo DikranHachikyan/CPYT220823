26bcf5fa6deefa0fbf52dfc2ffaa2ec9c7b5c24d
Dikran H <dhachikian@expert-bg.org>
2022-09-21 10:15:52 +0300

    Python L03

---
+++ ex13_zan03.py
@@ -0,0 +1,7 @@
+value = float(input('value='))
+
+x = value ** 2 if value > 0 else value * 2
+
+print(f'x={x}')
+
+print('---')
---
+++ ex17_zan03.py
@@ -0,0 +1,21 @@
+
+# print(f'name is:{__name__}')
+
+if __name__ == '__main__':
+    # 2 + 4 + ... + 98 + 100 = 2550
+    i = 1
+    sum_n = 0
+
+    while i <= 100:
+        i += 1
+
+        if i == 20:
+            break
+
+        sum_n += i    
+    else:
+        print('--- else ---')
+
+    print(f'2 + 4 +... + 98 + 100 = {sum_n}')
+
+    print('---')
